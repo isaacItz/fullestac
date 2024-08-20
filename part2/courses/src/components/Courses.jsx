391b017da95cdd11e39ab6a773d76c214af38330
Isaac Lugo <isaac.est.cbtis@gmail.com>
2024-08-20 09:39:14 -0600

    2.4 and 2.5

---
+++ part2/courses/src/components/Courses.jsx
@@ -0,0 +1,11 @@
+import Course from './Course';
+
+const Courses = ({ courses }) => {
+    return (
+        <>
+            {courses.map(c => <Course key={c.id} course={c} />)}
+        </>
+    )
+}
+
+export default Courses
